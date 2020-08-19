7588ef2046cc626c13308b1c7cf2bd24f2fa6241
wander <junjiehuang@yeah.net>
2020-08-19 10:20:21 +0800

    feature(14) longest_common_prefix

--- rust-leetcode/src/lib.rs
+++ rust-leetcode/src/lib.rs
@@ -1,3 +1,5 @@
 // ===================== Easy =====================
 #[path = "algo/roman_to_int.rs"]
 pub mod roman_to_int;
+#[path = "algo/longest_common_prefix.rs"]
+pub mod longest_common_prefix;
