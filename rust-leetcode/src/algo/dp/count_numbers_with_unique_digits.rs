1da9e692ce52f317a1cc84b22610cadbb0517a57
wander <junjiehuang@yeah.net>
2020-12-10 20:57:59 +0800

    fix

--- rust-leetcode/src/algo/dp/count_numbers_with_unique_digits.rs
+++ rust-leetcode/src/algo/dp/count_numbers_with_unique_digits.rs
@@ -1,17 +1,25 @@
 /// n, [0 ~ 10^n]
 pub fn run(n: i32) -> i32 {
-    let mut dp = vec![0; (n as usize) + 1];
 
-    dp[0] = 1;
-    dp[1] = 10;
-
-    for i in 2..=(n as usize) {
-
-        // 上一次的值 + (i - 1位数字的组合数)) * 可以插入的数字个数
-        dp[i] = dp[i - 1] + (dp[i - 1] - dp[i - 2]) * (10 - (i - 1))
+    if  n == 0 {
+      1
+    } else if n == 1 {
+      10
+    } else {
+      let mut dp = vec![0; (n as usize) + 1];
+  
+      dp[0] = 1;
+      dp[1] = 10;
+  
+      for i in 2..=(n as usize) {
+  
+          // 上一次的值 + (i - 1位数字的组合数)) * 可以插入的数字个数
+          dp[i] = dp[i - 1] + (dp[i - 1] - dp[i - 2]) * (10 - (i - 1))
+      }
+  
+      dp[n as usize] as i32
     }
 
-    dp[n as usize] as i32
 }
 
 #[cfg(test)]
