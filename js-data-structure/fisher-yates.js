224030f27a8dc7614158282cc189b9897980eaf7
wander <junjiehuang@yeah.net>
2021-04-20 15:40:07 +0800

    algo(fisher-yates) 置换算法、高纳德随机置换算法

---
+++ js-data-structure/fisher-yates.js
@@ -0,0 +1,28 @@
+// 随机置换算法
+//
+// 每次从剩余数字中选择一个数和最后的数置换，尾部长度减一。循环
+//
+function shuffle(arr) {
+  let pivot = arr.length;
+  while (pivot) {
+    let rand = Math.floor(Math.random() * pivot);
+
+    swap(rand, pivot - 1);
+    pivot--;
+  }
+
+  return arr;
+
+  function swap(i, j) {
+    let tmp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = tmp;
+  }
+}
+
+
+module.exports = shuffle;
+
+let arr = [0,1,2,3,4,5,6,7,8,9,10]
+
+console.log(shuffle(arr))
