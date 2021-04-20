9b7d8646e9617e0a05c824bed24e4a3225d12d2b
wander <junjiehuang@yeah.net>
2021-04-20 15:41:28 +0800

    algo(fisher-yates) 随机置换算法

--- js-data-structure/fisher-yates.js
+++ js-data-structure/fisher-yates.js
@@ -20,9 +20,8 @@ function shuffle(arr) {
   }
 }
 
-
 module.exports = shuffle;
 
-let arr = [0,1,2,3,4,5,6,7,8,9,10]
+let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-console.log(shuffle(arr))
+console.log(shuffle(arr));
